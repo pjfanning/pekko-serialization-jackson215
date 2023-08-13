711918e8ce8eed20028edccb72150661bdb99d5d
PJ Fanning <pjfanning@users.noreply.github.com>
2023-08-13 11:54:24 +0100

    more java tests

---
+++ src/test/java/jdoc/org/apache/pekko/serialization/jackson/v2a/CustomerMigration.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
+ */
+
+/*
+ * Copyright (C) 2019-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package jdoc.org.apache.pekko.serialization.jackson.v2a;
+
+// #structural
+import com.github.pjfanning.pekko.serialization.jackson215.JacksonMigration;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class CustomerMigration extends JacksonMigration {
+
+  @Override
+  public int currentVersion() {
+    return 2;
+  }
+
+  @Override
+  public JsonNode transform(int fromVersion, JsonNode json) {
+    ObjectNode root = (ObjectNode) json;
+    if (fromVersion <= 1) {
+      ObjectNode shippingAddress = root.with("shippingAddress");
+      shippingAddress.set("street", root.get("street"));
+      shippingAddress.set("city", root.get("city"));
+      shippingAddress.set("zipCode", root.get("zipCode"));
+      shippingAddress.set("country", root.get("country"));
+      root.remove("street");
+      root.remove("city");
+      root.remove("zipCode");
+      root.remove("country");
+    }
+    return root;
+  }
+}
+// #structural
---
+++ src/test/java/jdoc/org/apache/pekko/serialization/jackson/v2c/ItemAddedMigration.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
+ */
+
+/*
+ * Copyright (C) 2019-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package jdoc.org.apache.pekko.serialization.jackson.v2c;
+
+// #rename
+
+import com.github.pjfanning.pekko.serialization.jackson215.JacksonMigration;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class ItemAddedMigration extends JacksonMigration {
+
+  @Override
+  public int currentVersion() {
+    return 2;
+  }
+
+  @Override
+  public JsonNode transform(int fromVersion, JsonNode json) {
+    ObjectNode root = (ObjectNode) json;
+    if (fromVersion <= 1) {
+      root.set("itemId", root.get("productId"));
+      root.remove("productId");
+    }
+    return root;
+  }
+}
+// #rename
